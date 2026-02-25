17c02523ad23f1c2d262a9fd85ce6e3ae35e2463
Ernest Solomon <marveldad2@gmail.com>
2026-02-25 02:48:54 +0100

    models, migrations, admin setup, Django REST Framework(DRF) installed, requirements.txt added

---
+++ products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from categories.models import Category
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
